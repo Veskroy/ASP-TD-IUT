bb85cf8c671a9313a70f6d0343be2d6d94e2eaea
VINCENT KPATINDE <vincent.kpatinde@etudiant.univ-reims.fr>
2024-01-24 16:24:43 +0100

    Fin TP 1 Ajout - Application cliente d’API http

---
+++ MonApplicationCliente/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MonApplicationCliente
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5292");
+
+            HttpResponseMessage messageretour = await client.GetAsync("/Home/Index");
+
+            if (messageretour.IsSuccessStatusCode)
+            {
+                Console.WriteLine(messageretour.ToString());
+                Console.WriteLine(await messageretour.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                Console.WriteLine(messageretour.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
