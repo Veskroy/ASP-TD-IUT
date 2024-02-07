b66ce8a28d350b9400fdc146273982e8c0037e32
VINCENT KPATINDE <vincent.kpatinde@etudiant.univ-reims.fr>
2024-02-07 16:16:16 +0100

    Syntaxe Razor ajout

--- MonApplicationWebMVC/Models/Pays.cs
+++ MonApplicationWebMVC/Models/Pays.cs
@@ -2,8 +2,28 @@
 {
     public class Pays
     {
-      
 
+        public string Nom { get; set; }
+       public string Continent { get; set; }
+        public int Population{ get; set; }
+       public  string Drapeaux { get; set; }
+       public  float Superficie{ get; set;}
+
+        List<Pays> Lespays=new List<Pays>();
+
+
+        public static List<Pays> TousLesPays()
+        {
+            return new List<Pays>
+            {
+                new Pays{Nom="France",Superficie=672051,Continent="Europe",Population=68042591,Drapeaux="France.png" },
+            new Pays { Nom = "Mozambique", Superficie =8547404, Continent = "Afrique", Population =215002523, Drapeaux = "Mozambique.png" },
+            new Pays { Nom = "Brésil", Superficie = 8547404, Continent = "Amérique", Population =215002523, Drapeaux = "Brazil.png" },
+                new Pays{Nom="Allemagne",Superficie= 357588,Continent="Europe",Population=84358845,Drapeaux="Allemagne.png" },
+            };
+
+
+        }
 
     }
 }
