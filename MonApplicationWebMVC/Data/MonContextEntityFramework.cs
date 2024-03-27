cb4b797fa2a2a5c16a110f567b11b4f70eff8ba9
VINCENT KPATINDE <vincent.kpatinde@etudiant.univ-reims.fr>
2024-03-27 14:36:08 +0100

    Ajout de restriction Reglage a nos Data

--- MonApplicationWebMVC/Data/MonContextEntityFramework.cs
+++ MonApplicationWebMVC/Data/MonContextEntityFramework.cs
@@ -6,5 +6,11 @@ namespace MonApplicationWebMVC.Data
     public class MonContextEntityFramework : DbContext
     {
         public virtual DbSet<Pays> Pays { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pays>().HasKey(p => p.Id);
+            modelBuilder.Entity<Pays>().Ignore(p => p.ImportDrapeau);
+        }
     }
 }
