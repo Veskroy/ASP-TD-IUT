2d6d8bb22fe8cc7da30a2e5ff54960ea038cc4a4
VINCENT KPATINDE <vincent.kpatinde@etudiant.univ-reims.fr>
2024-03-27 15:24:59 +0100

    Partie Entity FrameWork effectuer.

--- MonApplicationWebMVC/Data/MonContextEntityFramework.cs
+++ MonApplicationWebMVC/Data/MonContextEntityFramework.cs
@@ -5,12 +5,38 @@ namespace MonApplicationWebMVC.Data
 {
     public class MonContextEntityFramework : DbContext
     {
+        private string Filepath;
+        public MonContextEntityFramework()
+        {
+            Filepath = "localDb.db";
+        }
+        // Ici un autre methode pour ajouter le Filpath mais que ne marche pas dans le context actuelle. 
+        //
+        //public MonContextEntityFramework(string filpath = null)
+        //{
+        //    if (filpath != null) { Filepath = filpath; }
+        //    else 
+        //    {
+        //        WebApplication app = builder.Build(); => A effectuer dasn le Program. cs qui defini builder.
+        //        string Filepath = Path.Combine(app.Environment.ContentRootPath,"localDb.db");
+        //       
+        //    }
+        //}
         public virtual DbSet<Pays> Pays { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Pays>().HasKey(p => p.Id);
             modelBuilder.Entity<Pays>().Ignore(p => p.ImportDrapeau);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={Filepath}");
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
---
+++ MonApplicationWebMVC/Migrations/20240327142258_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonApplicationWebMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Continent = table.Column<string>(type: "TEXT", nullable: false),
+                    Population = table.Column<int>(type: "INTEGER", nullable: false),
+                    Drapeaux = table.Column<string>(type: "TEXT", nullable: false),
+                    Superficie = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pays", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pays");
+        }
+    }
+}
